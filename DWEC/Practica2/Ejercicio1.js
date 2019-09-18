3e51a4dac2fa2a6d1e1f375a404d70317f082342
SergioZulueta <sergio.zulueta@ikasle.egibide.org>
2019-09-18 13:01:19 +0200

    Avance DEWC

---
+++ DWEC/Practica2/Ejercicio1.js
@@ -0,0 +1,36 @@
+//Codifica un ejemplo con cada una de las propiedades y/o metodos ´
+// de las clases String, Number y Date. Prueba tambien algunos de los ´
+// metodos de las clase Math.
+
+    let string = "palabra";
+
+document.write("Propiedades del String" + "<br>");
+
+document.write(string.anchor() + ".anchor sirve para crear y mostrar programando un ancla en un documento"+ "<br>");
+document.write(string.big()+ ".big sirve para formatear y mostrar una cadena en un documento"+ "<br>");
+document.write(string.blink()+ ".blank causa que una cadena parpadee como si estuviese en una etiqueta" + "<br>");
+document.write(string.bold()+ ".bold sirve para mostrar en negrita la cadena" + "<br>");
+document.write(string.charAt(2)+ ".charAt devuelve en un nuevo String el caracter que le decimos a traves de un numero entero" + "<br>");
+document.write(string.charCodeAt(2)+ ".charCodeAt devuelvo un numero indicando un valor unicode" + "<br>");
+document.write(string.codePointAt()+ ".codePointAt devuelve un entero no negativo que equivale al valor Unicode code point" + "<br>");
+document.write(string.concat()+ ".concat se utiliza para combinar dos o mas cadenas, no cambia las existentes, retorna una nueva" + "<br>");
+document.write(string.endsWith()+ ".endsWith determina cuando una cadena termina con los caracteres de otra cadena, devuelve true o false" + "<br>");
+document.write(string.fixed()+ ".fixed hace que muestre la cadena con una fuente de ancho fijo como si estuviera en las etiquetas tt" + "<br>");
+document.write(string.fontcolor("red")+ ".fontcolor('red') muestra la cadena en el color que se le indique, rojo en este caso " + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
+document.write(+ "" + "<br>");
