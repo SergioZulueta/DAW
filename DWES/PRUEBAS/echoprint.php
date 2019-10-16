211a2da48f1e490c28a73ca4388a3fcfa5961259
Sergio Zulueta <sergio.zulueta@ikasle.egibide.org>
2019-10-16 23:24:38 +0200

    php

---
+++ DWES/PRUEBAS/echoprint.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Hola mundo PHP</title>
+</head>
+<body>
+<?php
+echo ("<h2>Hola Mundo</h2>");
+echo "Hola, mundo!<br>";
+echo "Hola" , " mundo!";
+
+print("Hola, mundo!")
+?>
+</body>
+</html>
+
