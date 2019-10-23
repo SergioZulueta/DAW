a25f58e898ae5ca0deb1ed6172e2125b0348659c
Sergio <sergio.zulueta@ikasle.egibide.org>
2019-10-23 13:02:42 +0200

    formularios php inicio

---
+++ DWES/FORMULARIOS/ejercicio1.php
@@ -0,0 +1,28 @@
+<!--
+/*
+22. Crea una aplicación que genere un array con 20 números aleatorios con valores entre 1 y
+999, utilizando la función random_int(1, 999) para generar los valores. A continuación
+recorre el array utilizando la sentencia foreach y muestra por pantalla el valor más alto y el
+más bajo.
+*/
+-->
+
+<html>
+<head>
+    <title>Hola mundo PHP</title>
+</head>
+<body>
+
+<form method="get">
+
+    Introduce la temperatura: <input type="text" name="temperatura"><br>
+    Indica la unidad de la temperatura introducida:
+    <select>
+        <option value="celsius">Celsius</option>
+        <option value="fahrenheit">Fahrenheit</option>
+    </select>
+
+</form>
+
+</body>
+</html>
