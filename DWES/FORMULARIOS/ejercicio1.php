984ea2fa499facfae4d0034cb34997f2bda43381
Sergio <sergio.zulueta@ikasle.egibide.org>
2019-10-24 12:58:30 +0200

    php

--- DWES/FORMULARIOS/ejercicio1.php
+++ DWES/FORMULARIOS/ejercicio1.php
@@ -1,28 +1,56 @@
-<!--
-/*
-22. Crea una aplicación que genere un array con 20 números aleatorios con valores entre 1 y
-999, utilizando la función random_int(1, 999) para generar los valores. A continuación
-recorre el array utilizando la sentencia foreach y muestra por pantalla el valor más alto y el
-más bajo.
-*/
--->
+
 
 <html>
 <head>
     <title>Hola mundo PHP</title>
 </head>
+
+
+<?php
+
+
+function convertir(){
+
+    $temp = $_POST['temperatura'];
+    $unidad = $_POST['unidad'];
+    if ($temp == "") {
+        echo "<p>No has escrito la temperatura</p>";
+    } elseif ($unidad == 'c') {
+        $temp = $temp * 1.8 + 32;
+        echo "Los celsius a fahrenheit son: ".$temp."<p></p>";
+    } else {
+        $temp = $temp / 1.8 - 32;
+        echo "Los fahrenheit a celsius son: ".$temp."<p></p>";
+    }
+}
+
+
+
+?>
+
+
 <body>
 
-<form method="get">
+<?php
 
-    Introduce la temperatura: <input type="text" name="temperatura"><br>
-    Indica la unidad de la temperatura introducida:
-    <select>
-        <option value="celsius">Celsius</option>
-        <option value="fahrenheit">Fahrenheit</option>
-    </select>
+if (isset($_POST["convertir"])){
+    convertir();
+}
 
+?>
+
+<form action="ejercicio1.php" method="post">
+    <fieldset>
+        <legend>Conversor de temperatura</legend>
+        <strong>Introduce la temperatura:</strong>
+        <input type="number" name="temperatura" size="7" maxlength="7">
+        <select name="unidad">
+            <option value="c" selected="selected">Celsius</option>
+            <option value="f">Fahrenheit</option>
+        </select>
+
+        <p><input type="submit" value="convertir" name="convertir"></p>
+    </fieldset>
 </form>
-
 </body>
 </html>
