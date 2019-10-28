16b29b7253840c258dbdaeba5aa7ebb049f77929
Sergio <sergio.zulueta@ikasle.egibide.org>
2019-10-28 10:05:58 +0100

    DWEC

--- DWEC/Reto2/Practica1/ejercicio2.js
+++ DWEC/Reto2/Practica1/ejercicio2.js
@@ -1,6 +1,6 @@
 function añadir() {
     let body = document.documentElement.lastChild;
-    let ul = body.children[0];
+    let ul = body.children[1];
     let li = document.createElement('li');
     let texto = document.createTextNode(document.getElementById('text').value);
     let text = li.appendChild(texto);
---
+++ DWEC/Reto2/Practica2/Ejercicio1/reglasNavegacion.js
@@ -0,0 +1,10 @@
+function irAñadir(){
+    console.log("Ir ventana añadir")
+    location.href="añadir.html"
+}
+
+function irBusqueda() {
+    console.log("Ir ventana busqueda")
+    location.href="buscar.html"
+}
+
