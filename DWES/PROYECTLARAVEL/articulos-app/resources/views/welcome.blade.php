e6fb192969ba4b2095d151403765ed8e97975047
Sergio Zulueta <sergio.zulueta@ikasle.egibide.org>
2020-01-05 13:15:57 +0100

    Revert "make it better"
    This reverts commit 40308fd8ba27442fd63d3e9571718f5a939f782c.

--- DWES/PROYECTLARAVEL/articulos-app/resources/views/welcome.blade.php
+++ DWES/PROYECTLARAVEL/articulos-app/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hola
+                    Laravel
                 </div>
 
                 <div class="links">
